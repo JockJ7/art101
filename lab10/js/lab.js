0fbc2238d98ad38d855ea5ef5e49e2955f6ee800
JockJ7 <91556561+JockJ7@users.noreply.github.com>
2021-11-08 14:40:20 -0800

    hi

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -7,19 +7,14 @@
 *@since 10.20.21
 */
 function putNameInTitle() {
-  // When the button is pushed,
-  // prompt the user to input their name
-  // with the prompt() function.
-  //var name = prompt("Please enter your name:");
-  var field = document.getElementById("myfield");
+
+  var field = document.getElementById("mytextarea");
   var name = field.value;
 
-  // Use this input to change the text from
-  // ‘Hello, World!’ to ‘Hello’ + the user’s name
-  var headingEl = document.getElementById("title");
-  headingEl.innerHTML = "Hello, " + name;
+
+  var headingEl = document.getElementById("titlePart");
+  headingEl.innerHTML = "Hi there, " + name;
 }
 
-// Create an event handler within the JS file that listens for a button click.
 var buttonEl = document.getElementById("mybutton");
 buttonEl.addEventListener("click", putNameInTitle);
